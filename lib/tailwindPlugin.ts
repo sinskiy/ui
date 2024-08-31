2f45ea33f8917a4f5dee8982bb972e16b239e63f
fluqont <fluqont@gmail.com>
2024-08-31 09:14:07 +0300

    init: package.json

--- lib/tailwindPlugin.ts
+++
@@ -1,7 +0,0 @@
-import plugin from "tailwindcss/plugin";
-
-const tailwindPlugin = plugin(({ addVariant }) => {
-  addVariant("for-disabled", ["&:has(:disabled)", ".peer:disabled ~ &"]);
-});
-
-export { tailwindPlugin };
