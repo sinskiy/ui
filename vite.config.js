a298a5fa851da239f9ed3fb556703b4c3d328a35
fluqont <fluqont@gmail.com>
2024-08-31 13:38:46 +0300

    feat: use vite lib mode

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
 });
