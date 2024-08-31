0e2fec28eed76b2fa57f455887d7a26299980d00
sinskiy <sinskiy@proton.me>
2024-08-31 14:22:17 +0300

    feat: add autoprefixer and postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 import theme from "postcss-material-colors";
+import postcssPresetEnv from "postcss-preset-env";
+import autoprefixer from "autoprefixer";
 
 export default {
-  plugins: [theme({ primary: "#ff0000" })],
+  plugins: [theme({ primary: "#ff0000" }), postcssPresetEnv(), autoprefixer()],
 };
