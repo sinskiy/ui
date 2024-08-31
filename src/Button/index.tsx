bf6009374baab44c20d56a785ad89107c6c913f8
sinskiy <sinskiy@proton.me>
2024-08-31 13:27:24 +0300

    feat: add sass

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,3 +1,6 @@
+import "./index.sass";
+// TODO: test using button class in another component. if class is still applied, move to .module.sass
+
 export const Button = () => {
-  return <button>hello, world!</button>;
+  return <button className="button">hello, world!</button>;
 };
