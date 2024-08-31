b95ed40d2c99ee96ea2478420d358b2c06c5d651
sinskiy <sinskiy@proton.me>
2024-08-31 09:14:07 +0300

    init: package.json

--- lib/index.tsx
+++
@@ -1,9 +0,0 @@
-export * from "./Button";
-export * from "./Checkbox";
-export * from "./Input";
-export * from "./Kbd";
-export * from "./Label";
-export * from "./Skeleton";
-export * from "./Textarea";
-
-export * from "./utils";
