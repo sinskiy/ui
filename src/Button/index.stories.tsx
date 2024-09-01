e7aef98ef12fa1dab1133e969602db7af3fa3251
fluqont <fluqont@gmail.com>
2024-09-01 12:27:30 +0300

    feat: button

--- src/Button/index.stories.tsx
+++ src/Button/index.stories.tsx
@@ -7,12 +7,46 @@ const meta = {
   parameters: {
     layout: "centered",
   },
-  tags: ["autodocs"],
-  argTypes: {},
-  args: {},
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Primary: Story = {
+  args: {
+    themeColor: "primary",
+  },
+};
+export const Secondary: Story = {
+  args: {
+    themeColor: "secondary",
+  },
+};
+export const Tertiary: Story = {
+  args: {
+    themeColor: "tertiary",
+  },
+};
+export const Error: Story = {
+  args: {
+    themeColor: "error",
+  },
+};
+export const PrimaryContainer: Story = {
+  args: { themeColor: "primary-container" },
+};
+export const SecondaryContainer: Story = {
+  args: { themeColor: "secondary-container" },
+};
+export const TertiaryContainer: Story = {
+  args: { themeColor: "tertiary-container" },
+};
+export const ErrorContainer: Story = {
+  args: { themeColor: "error-container" },
+};
+export const SurfaceContainer: Story = {
+  args: { themeColor: "surface-container" },
+};
+export const SurfaceContainerHigh: Story = {
+  args: { themeColor: "surface-container-high" },
+};
--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,6 +1,13 @@
+import { InteractiveProps } from "../interactive";
 import "./index.sass";
 // TODO: test using button class in another component. if class is still applied, move to .module.sass
 
-export const Button = () => {
-  return <button className="button">hello, world!</button>;
+interface ButtonProps extends InteractiveProps {}
+
+export const Button = ({ themeColor = "primary" }: ButtonProps) => {
+  return (
+    <button className={classes(themeColor, "button")}>hello, world!</button>
+  );
 };
+
+const classes = (...passedClasses: string[]): string => passedClasses.join(" ");
