37749661b6f38111f2d2017190f979cae47b1ea4
sinskiy <sinskiy@proton.me>
2025-01-10 19:48:25 +0300

    fix: export SelectInput

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import "./reset.css";
 import "./index.css";
 import "./components.css";
-export * from "./Form/Form.tsx";
-export * from "./InputField/InputField.tsx";
-export * from "./Dropdown/Dropdown.tsx";
+export * from "./Form/Form";
+export * from "./InputField/InputField";
+export * from "./Dropdown/Dropdown";
+export * from "./SelectInput/SelectInput";
 export * from "./utils";
