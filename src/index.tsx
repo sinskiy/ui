39541f0f38953a83e8ef1f784785a5d41340acc3
sinskiy <sinskiy@proton.me>
2025-01-10 17:00:36 +0300

    feat: dropdown

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ import "./index.css";
 import "./components.css";
 export * from "./Form/Form.tsx";
 export * from "./InputField/InputField.tsx";
+export * from "./Dropdown/Dropdown.tsx";
 export * from "./utils";
